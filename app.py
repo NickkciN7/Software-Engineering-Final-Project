aae1070a914e759f5500e0ab3ff25794028efb3c
minxia dong <minxiadong@minqiangdeMacBook-Pro.local>
2022-04-02 15:46:51 -0400

    update login and signup backend

--- app.py
+++ app.py
@@ -3,12 +3,32 @@ import flask
 from flask import Flask, render_template, session
 
 import flask_login
-from flask_login import current_user, login_required
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    UserMixin,
+    login_required,
+)
+
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 
+# MD - information only
+# https://git.heroku.com/the-pokemasters.git
+# https://the-pokemasters.herokuapp.com/
+
 load_dotenv(find_dotenv())
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8zkdifenrk/ec]/'
+
+# MD - Old setting from Milestone2
+# uri = os.getenv("DATABASE_URL")
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
+# app.config["SQLALCHEMY_DATABASE_URI"] = uri
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 # Point SQLAlchemy to your Heroku database
@@ -22,7 +42,12 @@ if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
 # Gets rid of a warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
 
 
 class profile(flask_login.UserMixin, db.Model):
@@ -36,9 +61,65 @@ class profile(flask_login.UserMixin, db.Model):
 db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return profile.query.get(user_id)
+
+
 @app.route("/")
 def index():
     return "<h1>Welcome To Our Webpage for PokeMasters!!</h1>"
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if flask.request.method == "POST":
+        user_name = flask.request.form["user_name"]
+        password = flask.request.form["password"]
+
+        found_user = (
+            profile.query.filter_by(username=user_name)
+            .filter_by(password=password)
+            .first()
+        )
+        if found_user:
+            flask.flash(f"User Name {user_name} already exists!")
+            return flask.redirect("/signup")
+        else:
+            user = profile(username=user_name, password=password)
+            db.session.add(user)
+            db.session.commit()
+            flask.flash(f"{user_name} has been added")
+            return flask.redirect("/signup")
+    else:
+        return flask.render_template("signup.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if flask.request.method == "GET":
+        return flask.render_template("login.html")
+    else:
+        user_name = flask.request.form["user_name"]
+        password = flask.request.form["password"]
+
+        found_user = (
+            profile.query.filter_by(username=user_name)
+            .filter_by(password=password)
+            .first()
+        )
+        if found_user:
+            login_user(found_user)
+            return flask.redirect("/")
+        else:
+            flask.flash("This user does not exist!")
+            return flask.redirect("/login")
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    logout_user()
+    return flask.redirect("/login")
 
 
 app.run(
