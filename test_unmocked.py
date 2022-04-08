ed003c34af33d2d94e6bc12153652692237ad85c
Dominick DiLeo <nickkcindi@outlook.com>
2022-04-08 18:46:16 -0400

    added another unmocked unit test

--- test_unmocked.py
+++ test_unmocked.py
@@ -1,5 +1,6 @@
 import unittest
-from pokeapi import get_name
+from pokeapi import get_name, get_sprite
+
 
 class TestPokeName(unittest.TestCase):
     def test_poke_name(self):
@@ -7,6 +8,13 @@ class TestPokeName(unittest.TestCase):
         expected_output = "bulbasaur"
         output = get_name(id)
         self.assertEqual(expected_output, output)
+
+    def test_poke_sprite(self):
+        id = "4"
+        expected_output = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+        output = get_sprite(id)
+        self.assertEqual(expected_output, output)
+
 
 if __name__ == "__main__":
     unittest.main()
